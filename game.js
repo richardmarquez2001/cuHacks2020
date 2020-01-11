ba4944fcc6e8d13b46ce65a76451812cf7bbaf2a
Richard Marquez <richard.marquez.2001@gmail.com>
2020-01-11 17:26:30 -0500

    fixed dylans files bc hes dumb

--- game.js
+++ game.js
@@ -1,17 +1,33 @@
 $(document).ready(function() {
-  let str = "";
-  let id = "";
-  for (let i = 0; i < 16; i++) {
-    str += "<tr>";
+    let str = "";
+    let id = "";
+    let baseColor = "red";
+    let newColor = "blue";
+    let gridNum = 16;
+    for (let i = 0; i < gridNum; i++) {
+        str += "<tr>";
 
-    for (let j = 0; j < 16; j++) {
-      id = i.toString() + "-" + j.toString();
+        for (let j = 0; j < gridNum; j++) {
+            id = i.toString() + "-" + j.toString();
 
-      str += "<td id = '" + id + "'></td>";
+            str += "<td id = '" + id + "'></td>";
+        }
+
+        str += "</tr>";
     }
 
-    str += "</tr>";
-  }
+    $("#grid-container").html(str);
 
-  $("#grid-container").html(str);
+    $("td").css("background-color", baseColor);
+
+    $("td").mousedown(function(e){
+        switch(e.which){
+            case 1:
+                $(this).css("background-color", newColor);
+                break;
+            case 3:
+                $(this).css("background-color", baseColor);
+        }
+    })
+
 });
--- main.js
+++ main.js
@@ -1,63 +1,20 @@
-let $canvas_1 = $("#canvas_1");
-let $canvas_2 = $("#canvas_2");
+$(document).ready(function(){
 
-$canvas_1.css({
-    "border-color": "red",
-    "border-style": "solid",
-    "border-width": "5px"
-});
+    let str = "";
+    let id = "";
+    for (let i = 0; i < 16; i ++){
+        str += "<tr>";
 
-$canvas_2.css({
-    "border-color": "black",
-    "border-style": "solid",
-    "border-width": "5px"
-});
+        for (let j = 0; j < 16; j ++){
 
-$canvas_1.drawRect({
-    fillStyle: 'steelblue',
-    strokeStyle: 'blue',
-    strokeWidth: 4,
-    x: 150, y: 100,
-    fromCenter: false,
-    width: 200,
-    height: 100
-});
+            id = i.toString() + "-" + j.toString();
 
-$canvas_2.drawRect({
-    fillStyle: 'steelblue',
-    strokeStyle: 'blue',
-    strokeWidth: 4,
-    x: 150, y: 100,
-    fromCenter: false,
-    width: 200,
-    height: 100
-});
-
-$("#getAmount").click(function(){
-    let percent = calculatePercent();
-    $("#percent").html(percent);
-});
-
-function calculatePercent(){
-
-    const canvas1 = document.getElementById("canvas_1");
-    const canvas2 = document.getElementById("canvas_2");
-
-    let c1 = canvas1.getContext("2d");
-    let c2 = canvas2.getContext("2d");
-    let num = 0;
-
-    let imgData1 = c1.getImageData(0, 0, 500, 500);
-    let imgData2 = c2.getImageData(0, 0, 500, 500);
-    for(i = 0; i<= imgData1.data.length; i+= 4){
-        if (imgData1.data[i] === imgData2.data[i] && imgData1.data[i + 1] === imgData2.data[i + 1] &&
-            imgData1.data[i + 2] === imgData2.data[i + 2] && imgData1.data[i + 3] === imgData2.data[i + 3]){
-            num += 1;
+            str += "<td id = \'" + id + "\'></td>"
         }
+
+        str += "</tr>";
     }
-    val = Math.floor((num / (imgData1.data.length / 4)) * 100);
 
-    return val.toString() + "%";
+    $("#grid-container").html(str);
 
-}
-
+});
